b8680c000117610ef35485f127b69d1585647a6c
adelpal95 <adelpal95@gmail.com>
2026-01-08 23:40:42 +0200

    Lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,22 @@
-﻿namespace PalAdel7
+﻿using PalAdel7.Data;
+namespace PalAdel7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
